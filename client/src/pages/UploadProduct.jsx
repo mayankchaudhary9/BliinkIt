1244ca03fbbc9055e0f534e54ea9646ebc0506e6
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2025-12-30 23:50:20 +0530

    .....

--- client/src/pages/UploadProduct.jsx
+++ client/src/pages/UploadProduct.jsx
@@ -1,7 +1,99 @@
-import React from "react";
+import React, { useState } from "react";
+import { FaCloudUploadAlt } from "react-icons/fa";
 
 const UploadProduct = () => {
-  return <div>UploadProduct</div>;
+  const [data, setData] = useState({
+    name: "",
+    image: [],
+    category: [],
+    subCategory: [],
+    unit: [],
+    stock: "",
+    price: "",
+    discount: "",
+    description: "",
+    more_details: {},
+  });
+
+  const handleOnChange = (e) => {
+    const { name, value } = e.target;
+    setData((prev) => {
+      return {
+        ...prev,
+        [name]: value,
+      };
+    });
+  };
+
+  const handleUploadImage = (e) => {
+    const file = e.target.files[0];
+
+    if (!file) {
+      return;
+    }
+    console.log(file, "drfgbhjnk");
+  };
+  return (
+    <section>
+      <div className="p-2 bg-white shadow-md flex items-center justify-between">
+        <h2 className="font-semibold">Upload Product</h2>
+      </div>
+      <div className="grid p-3">
+        <form className="grid gap-2">
+          <div className="grid gap-1">
+            <label htmlFor="name">Name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Enter product name"
+              name="name"
+              value={data.name}
+              onChange={handleOnChange}
+              required
+              className="bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded"
+            />
+          </div>
+          <div className="grid gap-1">
+            <label htmlFor="description">Description</label>
+            <textarea
+              id="description"
+              type="text"
+              placeholder="Enter product description"
+              name="description"
+              value={data.description}
+              onChange={handleOnChange}
+              required
+              multiple
+              rows={3}
+              className="bg-blue-50 p-2 outline-none border focus-within:border-primary-200 rounded resize-none"
+            />
+          </div>
+          <div>
+            <p>Image</p>
+            <div>
+              <label
+                htmlFor="ProductImage"
+                className="bg-blue-50 h-24 border rounded flex justify-center items-center cursor-pointer"
+              >
+                <div className="flex justify-center items-center flex-col">
+                  <FaCloudUploadAlt size={35} />
+                  <p>Upload Image</p>
+                </div>
+                <input
+                  id="ProductImage"
+                  type="file"
+                  className="hidden"
+                  accept="image/*"
+                  onChange={handleUploadImage}
+                />
+              </label>
+              <div>{/* display uploaded images */}</div>
+            </div>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 };
 
 export default UploadProduct;
