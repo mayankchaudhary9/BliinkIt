1148649331c2eb1f55441755cf7445f32f063c56
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-01-05 22:26:43 +0530

    ....

--- server/models/product.model.js
+++ server/models/product.model.js
@@ -37,7 +37,7 @@ const productSchema = new mongoose.Schema(
       type: Number,
       default: null,
     },
-    discription: {
+    description: {
       type: String,
       default: "",
     },
