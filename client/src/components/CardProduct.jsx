d885d5722a9710bd5d0da8d94499443c87d274fc
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-01-20 23:53:16 +0530

    ....

--- client/src/components/CardProduct.jsx
+++ client/src/components/CardProduct.jsx
@@ -8,23 +8,25 @@ const CardProduct = ({ data }) => {
   return (
     <Link
       to={url}
-      className="border p-4 grid gap-3 max-w-52 lg:min-w-52 rounded"
+      className="border py-2 lg:p-4 grid gap-1 lg:gap-3 min-w-36 lg:min-w-52 rounded"
     >
-      <div className="min-h-20 max-w-32 rounded">
+      <div className="min-h-20 w-full max-h-24 lg:max-h-32 rounded overflow-hidden">
         <img
           src={data.image[0]}
-          className="w-full h-full object-scale-down scale-125"
+          className="w-full h-full object-scale-down lg:scale-125"
         />
       </div>
-      <div className="rounded text-sm w-fit p-[1px] px-2 text-green-600 bg-green-50">
+      <div className="rounded text-xs w-fit p-[1px] px-2 text-green-600 bg-green-50">
         10 min
       </div>
-      <div className="font-medium text-ellipsis line-clamp-2">{data.name}</div>
-      <div className="w-fit">{data.unit}</div>
+      <div className="px-2 lg:px-0 font-medium text-ellipsis text-sm lg:text-base line-clamp-2">
+        {data.name}
+      </div>
+      <div className="w-fit px-2 lg:px-0 text-sm lg:text-base">{data.unit}</div>
 
-      <div className="flex items-center justify-between gap-3">
+      <div className="px-2 lg:px-0 flex items-center justify-between gap-1 lg:gap-3 text-sm lg:text-base">
         <div className="font-semibold">{DisplayPriceInRupee(data.price)}</div>
-        <div className="bg-green-600 hover:bg-green-700 text-white px-4 py-1 rounded">
+        <div className="bg-green-600 hover:bg-green-700 text-white px-2 lg:px-4 py-1 rounded">
           <button>Add</button>
         </div>
       </div>
