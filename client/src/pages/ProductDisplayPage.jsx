cf6e1ac0d62fec12fd5a01968d61c01c72e9b888
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-01-28 00:47:07 +0530

    ...

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -11,6 +11,7 @@ const ProductDisplayPage = () => {
     name: "",
     image: [],
   });
+  const [image, setImage] = useState(0);
   const [loading, setLoading] = useState(false);
 
   const fetchProductDetails = async () => {
@@ -37,7 +38,51 @@ const ProductDisplayPage = () => {
   useEffect(() => {
     fetchProductDetails();
   }, [params]);
-  return <div>ProductDisplayPage</div>;
+  return (
+    <section className="container mx-auto p-4 grid lg:grid-cols-2">
+      <div className="">
+        <div className="bg-white lg:min-h-[65vh] lg:max-h-[65vh] rounded min-h-56 max-h-56 h-full w-full">
+          <img
+            src={data.image[image]}
+            className="w-full h-full object-scale-down"
+          />
+        </div>
+        <div className="flex items-center justify-center gap-3 my-2">
+          {data.image.map((img, index) => {
+            return (
+              <div
+                key={img + index + "points"}
+                className={`bg-slate-200 w-3 h-3 lg:w-5 lg:h-5 rounded-full ${index === image && "bg-slate-300"}`}
+              ></div>
+            );
+          })}
+        </div>
+        <div className="grid relative">
+          <div className="flex gap-4 w-full overflow-x-auto scrollbar-none">
+            {data.image.map((img, index) => {
+              return (
+                <div
+                  key={img + index}
+                  className="w-20 h-20 min-h-20 min-w-20 cursor-pointer shadow-md"
+                >
+                  <img
+                    src={img}
+                    alt="min-product"
+                    onClick={() => setImage(index)}
+                    className="w-full h-full object-scale-down"
+                  />
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            <button></button>
+          </div>
+        </div>
+      </div>
+      <div></div>
+    </section>
+  );
 };
 
 export default ProductDisplayPage;
