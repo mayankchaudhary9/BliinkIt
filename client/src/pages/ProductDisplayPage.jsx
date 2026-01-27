e113925041ae18ea61d3f5442cf5a94564c97222
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-01-27 23:58:47 +0530

    ....

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -1,9 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import AxiosToastError from "../utils/AxiosToastError";
+import Axios from "../utils/Axios";
+import SummaryApi from "../common/SummaryApi";
 
 const ProductDisplayPage = () => {
   const params = useParams();
   let productId = params?.product?.split("-")?.slice(-1)[0];
+  const [data, setData] = useState({
+    name: "",
+    image: [],
+  });
+  const [loading, setLoading] = useState(false);
+
+  const fetchProductDetails = async () => {
+    try {
+      setLoading(true);
+      const response = await Axios({
+        ...SummaryApi.getProductDetails,
+        data: {
+          productId: productId,
+        },
+      });
+
+      const { data: responseData } = response;
+      if (responseData.success) {
+        setData(responseData.data);
+      }
+    } catch (error) {
+      AxiosToastError(error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductDetails();
+  }, [params]);
   return <div>ProductDisplayPage</div>;
 };
 
