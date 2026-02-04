77df78a84af19fc40a523c74dbfad381b744c511
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-02-04 23:46:35 +0530

    ......

--- client/src/pages/ProductDisplayPage.jsx
+++ client/src/pages/ProductDisplayPage.jsx
@@ -9,6 +9,7 @@ import Divider from "../components/Divider";
 import image1 from "../assets/minute_delivery.png";
 import image2 from "../assets/Best_Prices_Offers.png";
 import image3 from "../assets/Wide_Assortment.png";
+import { priceWithDiscount } from "../utils/PriceWithDiscount";
 
 const ProductDisplayPage = () => {
   const params = useParams();
@@ -117,15 +118,30 @@ const ProductDisplayPage = () => {
         <Divider />
         <div>
           <p className="">Price</p>
-          <div className="border border-green-600 px-4 py-2 rounded bg-green-50 w-fit">
-            <p className="font-semibold text-lg lg:text-xl">
-              {DisplayPriceInRupee(data.price)}
-            </p>
+          <div className="flex items-center gap-2 lg:gap-4">
+            <div className="border border-green-600 px-4 py-2 rounded bg-green-50 w-fit">
+              <p className="font-semibold text-lg lg:text-xl">
+                {DisplayPriceInRupee(
+                  priceWithDiscount(data.price, data.discount),
+                )}
+              </p>
+            </div>
+
+            {data.discount && (
+              <p className="line-through">{DisplayPriceInRupee(data.price)}</p>
+            )}
+
+            {data.discount && (
+              <p className="font-bold text-green-600 lg:text-2xl">
+                {data.discount}%{" "}
+                <span className="text-base text-neutral-500">Discount</span>
+              </p>
+            )}
           </div>
         </div>
 
         {data.stock === 0 ? (
-          <p className="text-lg text-red-500">Out of Stock</p>
+          <p className="text-lg text-red-500 my-2">Out of Stock</p>
         ) : (
           <button className="my-4 px-4 py-1 bg-green-600 hover:bg-green-700 text-white rounded">
             Add
