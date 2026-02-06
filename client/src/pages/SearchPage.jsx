bb80140b25e21470ef81b4b876d473a61c10fc20
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-02-06 23:59:31 +0530

    .....

--- client/src/pages/SearchPage.jsx
+++ client/src/pages/SearchPage.jsx
@@ -1,7 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
+import CardLoading from "../components/CardLoading";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  const [data, setData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const loadingArrayCard = new Array(10).fill(null);
+  return (
+    <section className="bg-white">
+      <div className="container mx-auto p-4">
+        <p className="font-semibold">Search Results: {data.length}</p>
+
+        <div>
+          {/* loading data */}
+          {loading &&
+            loadingArrayCard.map((_, index) => {
+              return <CardLoading key={"loadingsearchpage" + index} />;
+            })}
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default SearchPage;
