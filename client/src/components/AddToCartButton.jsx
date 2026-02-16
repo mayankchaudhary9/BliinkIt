abdb4f96582fe95ae24e00f6bd69dfc55184a7dc
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-02-16 23:48:15 +0530

    ....

--- client/src/components/AddToCartButton.jsx
+++ client/src/components/AddToCartButton.jsx
@@ -1,14 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { useGlobalContext } from "../provider/GlobalProvider";
 import Axios from "../utils/Axios";
 import SummaryApi from "../common/SummaryApi";
 import toast from "react-hot-toast";
 import AxiosToastError from "../utils/AxiosToastError";
 import Loading from "./Loading";
+import { useSelector } from "react-redux";
+import { FaMinus, FaPlus } from "react-icons/fa6";
 
 const AddToCartButton = ({ data }) => {
   const [loading, setLoading] = useState(false);
   const { fetchCartItem } = useGlobalContext();
+  const cartItem = useSelector((state) => state.cartItem.cart);
+  const [itemSelected, setItemSelected] = useState(false);
 
   const handleAddToCart = async (e) => {
     // for stopping redirect after click on add button
@@ -38,14 +42,34 @@ const AddToCartButton = ({ data }) => {
       setLoading(false);
     }
   };
+
+  //checking this item in cart or not
+  useEffect(() => {
+    const checkingItem = cartItem.some(
+      (item) => item.productId._id === data._id,
+    );
+    setItemSelected(checkingItem);
+  }, [data, cartItem]);
   return (
     <div>
-      <button
-        onClick={handleAddToCart}
-        className="bg-green-600 hover:bg-green-700 text-white px-2 lg:px-4 py-1 rounded"
-      >
-        {loading ? <Loading /> : "Add"}
-      </button>
+      {itemSelected ? (
+        <div>
+          <button>
+            <FaMinus />
+          </button>
+          <p></p>
+          <button>
+            <FaPlus />
+          </button>
+        </div>
+      ) : (
+        <button
+          onClick={handleAddToCart}
+          className="bg-green-600 hover:bg-green-700 text-white px-2 lg:px-4 py-1 rounded"
+        >
+          {loading ? <Loading /> : "Add"}
+        </button>
+      )}
     </div>
   );
 };
