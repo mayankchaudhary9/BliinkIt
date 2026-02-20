17eb31530649c0824588353a150838d5f189c796
mayankchaudhary9 <chaudharymayank5800@gmail.com>
2026-02-20 23:52:04 +0530

    .....

--- client/src/components/DisplayCartItem.jsx
+++ client/src/components/DisplayCartItem.jsx
@@ -4,7 +4,20 @@ import { Link } from "react-router-dom";
 
 const DisplayCartItem = ({ close }) => {
   return (
-    
+    <section className="bg-neutral-900/70 fixed top-0 bottom-0 right-0 left-0 z-50">
+      <div className="bg-white w-full max-w-sm min-h-screen max-h-screen ml-auto">
+        <div className="flex items-center p-4 shadow-md gap-3 justify-between">
+          <h2 className="font-semibold">Cart</h2>
+          <Link to={"/"} className="lg:hidden">
+            <IoClose size={25} />
+          </Link>
+          <button onClick={close} className="hidden lg:block">
+            <IoClose size={25} />
+          </button>
+          {/* 9:9:00 */}
+        </div>
+      </div>
+    </section>
   );
 };
 
